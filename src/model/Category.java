2eebfb1cbb760857fdc20d4a5c7feb8fb2f76417
MineKnight2711 <witherdragonlair@gmail.com>
2023-10-25 21:21:53 +0700

    init git

---
+++ src/model/Category.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author WitherDragon
+ */
+public class Category {
+    private String categoryID;
+    private String categoryName;
+    private String imageUrl;
+
+    public String getCategoryID() {
+        return categoryID;
+    }
+
+    public void setCategoryID(String categoryID) {
+        this.categoryID = categoryID;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+    
+}
---
+++ src/model/ResponseModel.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.UUID;
+
+/**
+ *
+ * @author WitherDragon
+ */
+public class ResponseModel {
+    private UUID id;
+    private String apiVersion;
+    private String message;
+    private Object data;
+    
+    public ResponseModel(String message, Object data) {
+        this.id = UUID.randomUUID();
+        this.apiVersion = "1.0";
+        this.message = message;
+        this.data = data;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getApiVersion() {
+        return apiVersion;
+    }
+
+    public void setApiVersion(String apiVersion) {
+        this.apiVersion = apiVersion;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+    
+    
+}
